f57b64ff6f1b1f779f261ae8ca92b4e382fd6908
841 <q5078637@gmail.com>
2024-07-06 00:49:51 +0900

    a

--- c/test.c
+++ c/test.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(void){
-    printf("Hello\n");
+    int a[3];
+    int j=1;
+    a[j]=4;
+    //a[j]==*(&a[0]+j)==*(a+j)==*(j+a)==j[a]
+    printf("%d\n",j[a]);
     return 0;
 }
